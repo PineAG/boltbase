c8241b39c84244a56323b732d894ef9c718c98cd
PineAG <pein.agail@gmail.com>
2022-12-10 13:13:55 +0800

    fix check origin

--- boltbase.go
+++ boltbase.go
@@ -13,7 +13,14 @@ import (
 	"github.com/gorilla/websocket"
 )
 
-var upgrader = websocket.Upgrader{} // use default options
+var upgrader = websocket.Upgrader{
+	ReadBufferSize:  1024,
+	WriteBufferSize: 1024,
+	// 解决跨域问题
+	CheckOrigin: func(r *http.Request) bool {
+		return true
+	},
+} // use default options
 
 func registerWebSocket(path string, ws *websocket.Conn) {
 
